ac5ca07bd084d489a4025ceb2ffce0333cb2d7ee
GabrielF <ogabrielfef@gmail.com>
2022-06-27 16:35:06 -0300

    req 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import Loading from '../Loading';
 
 class MusicCard extends React.Component {
   state = {
     loading: false,
+    checked: false,
   }
 
+  componentDidMount() {
+    this.fetchFavoriteSongs();
+  }
+
+  fetchAddSong = async () => {
+    const { checked } = this.state;
+    const { musicObj, fetchFavorites } = this.props;
+    this.setState({ loading: true });
+    await addSong(musicObj);
+    if (!checked) {
+      await removeSong(musicObj);
+      fetchFavorites();
+    }
+    this.setState({ loading: false });
+  }
+
+  fetchFavoriteSongs = async () => {
+    const { trackName } = this.props;
+    this.setState({ loading: true });
+    const favorites = await getFavoriteSongs();
+    const someFavorite = favorites.some((favorite) => favorite.trackName === trackName);
+    this.setState({ checked: someFavorite, loading: false });
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ checked: target.checked }, this.fetchAddSong);
+  };
+
   render() {
-    const { loading } = this.state;
-    const { trackName, previewUrl } = this.props;
+    const { loading, checked } = this.state;
+    const { trackName, previewUrl, trackId } = this.props;
     return (
       <div>
         {loading && <Loading />}
@@ -19,6 +49,16 @@ class MusicCard extends React.Component {
           O seu navegador não suporta o elemento
           <code>audio</code>
         </audio>
+        <label htmlFor="favorite">
+          Favorita
+          <input
+            id="favorite"
+            checked={ checked }
+            onChange={ this.handleChange }
+            type="checkbox"
+            data-testid={ `checkbox-music-${trackId}` }
+          />
+        </label>
       </div>
     );
   }
@@ -29,5 +69,9 @@ MusicCard.propTypes = {
   previewUrl: PropTypes.string,
   trackId: PropTypes.string,
 }.isRequired;
+
+MusicCard.defaultProps = {
+  fetchFavorites: () => {},
+};
 
 export default MusicCard;
