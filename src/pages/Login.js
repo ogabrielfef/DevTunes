fd910f60df285e65ff98a1beff3a72251ad54b01
GabrielF <ogabrielfef@gmail.com>
2022-06-27 09:21:07 -0300

    lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
+
+const minimunCaracters = 3;
 
 class Login extends Component {
+  state = {
+    name: '',
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value });
+  }
+
   render() {
+    const { name } = this.state;
     return (
       <div data-testid="page-login">
         <h1>Login</h1>
+        <label htmlFor="name">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={ name }
+            data-testid="login-name-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ name.length <= minimunCaracters }
+          onClick={ this.handleClick }
+        >
+          Entrar
+
+        </button>
       </div>
     );
   }
