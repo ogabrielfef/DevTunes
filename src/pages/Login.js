ea629124e49d3949a0359242b6c2ef0919a4c022
GabrielF <ogabrielfef@gmail.com>
2022-06-28 03:15:56 -0300

    erro encontrado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { createUser } from '../services/userAPI';
 import Loading from '../Loading';
 
@@ -9,51 +9,58 @@ class Login extends Component {
   state = {
     name: '',
     loading: false,
-    fetchState: false,
   }
-
-  fetchApi = async () => {
-    const { name } = this.state;
-    this.setState({ loading: true });
-    await createUser({ name });
-    this.setState({ loading: false, fetchState: true });
-    console.log(name);
-    console.log(createUser);
-  };
 
   handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value });
   }
 
+  saved = async (event) => {
+    event.preventDefault();
+    const { name } = this.state;
+    const { history: { push } } = this.props;
+    this.setState({ loading: true });
+    await createUser({ name });
+    push('/search');
+  }
+
   render() {
-    const { name, loading, fetchState } = this.state;
+    const { name, loading } = this.state;
     return (
-      <div data-testid="page-login">
-        <h1>Login</h1>
-        <label htmlFor="name">
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={ name }
-            data-testid="login-name-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <button
-          type="button"
-          data-testid="login-submit-button"
-          onClick={ this.fetchApi }
-          disabled={ name.length < minimunCaracters }
-        >
-          Entrar
-        </button>
-        {loading && <Loading />}
-        {fetchState && <Redirect to="/search" />}
+      <div>
+        {loading ? <Loading /> : (
+          <div data-testid="page-login">
+            <h1>Login</h1>
+            <label htmlFor="name">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={ name }
+                data-testid="login-name-input"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              onClick={ this.saved }
+              disabled={ name.length < minimunCaracters }
+            >
+              Entrar
+            </button>
+          </div>
+        )}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
