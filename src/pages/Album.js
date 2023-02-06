cf730e9d5887f7059711e64da4f3a29d5ec5eca0
GabrielF <ogabrielfef@gmail.com>
2023-02-05 22:56:06 -0300

    css na pagina do album selecionado

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
 import Loading from '../Loading';
+import './Album.css';
 
 class Album extends Component {
   state = {
@@ -26,25 +27,31 @@ class Album extends Component {
     return (
       <div data-testid="page-album">
         <Header />
-        <h1>Album</h1>
-        {loading
-          ? (<Loading />)
-          : (
-            <>
-              <p data-testid="album-name">{`Album: ${musicsArray[0].collectionName}`}</p>
-              <p data-testid="artist-name">{`Artist: ${musicsArray[0].artistName}`}</p>
-            </>
-          )}
-        {musicsArray.filter((music) => music.trackName).map((music) => (
-          <div key={ music.trackId }>
-            <MusicCard
-              trackId={ music.trackId }
-              trackName={ music.trackName }
-              previewUrl={ music.previewUrl }
-              musicObj={ music }
-            />
+        <div className="selected-album">
+          {loading
+            ? (<Loading />)
+            : (
+              <div className="album-info">
+                <img alt="capa do album" src={ `${musicsArray[0].artworkUrl100}` } />
+                <p data-testid="album-name">
+                  {`${musicsArray[0].collectionName}`}
+                </p>
+                <p data-testid="artist-name">{`${musicsArray[0].artistName}`}</p>
+              </div>
+            )}
+          <div className="music-list">
+            {musicsArray.filter((music) => music.trackName).map((music) => (
+              <div key={ music.trackId }>
+                <MusicCard
+                  trackId={ music.trackId }
+                  trackName={ music.trackName }
+                  previewUrl={ music.previewUrl }
+                  musicObj={ music }
+                />
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     );
   }
